05d0971770eb085751171f85dc48feaa42519120
Navid Akram <navid.youtub@gmail.com>
2025-07-27 00:15:16 +0100

    1

--- metro.config.js
+++ metro.config.js
@@ -11,4 +11,18 @@ config.resolver.alias = {
   'react-native$': 'react-native-web',
 };
 
+// Add better file watching configuration
+config.watchFolders = [__dirname];
+config.resolver.nodeModulesPaths = [__dirname + '/node_modules'];
+
+// Configure file watching to ignore problematic directories
+config.resolver.blockList = [
+  /node_modules\/.*\/example/,
+  /node_modules\/.*\/test/,
+  /node_modules\/.*\/tests/,
+];
+
+// Add error handling for file watching
+config.resolver.enableGlobalPackages = true;
+
 module.exports = config;
